8324a9f7c9a32fff768e4f1b3ad5218dba7bd9fd
Jan Provaznik <jan@provaznik.pro>
2022-03-17 11:57:48 +0100

    Introduced examples.

---
+++ examples/simple/concurrent/parallel.py
@@ -0,0 +1,69 @@
+import numpy
+import nomadlad
+
+import functools
+import concurrent.futures
+
+# Blackbox function. 
+#
+# Given a single numpy.ndarray point to evaluate the blackbox at.
+# Returns (success, include, outcome) triplet.
+#
+# Value of 'outcome' must follow the 'BB_OUTPUT_TYPE' option!
+
+def blackbox (point):
+    fw = numpy.linalg.norm(point - 1, 2)
+    cw = 2 - numpy.linalg.norm(point, 2)
+
+    success = 1
+    include = 1
+    outcome = '{:.16f} {:.16f}'.format(fw, cw)
+
+    return success, include, outcome
+
+# Parameters for the NOMAD optimization engine.
+# Note BB_MAX_BLOCK_SIZE, BB_OUTPUT_TYPE and MAX_BB_EVAL options.
+    
+parameters = [
+    'BB_OUTPUT_TYPE OBJ PB',
+    'BB_MAX_BLOCK_SIZE 10',
+    'MAX_BB_EVAL 1000',
+
+    'SEED {}'.format(numpy.random.randint(0, 2 ** 31)),
+    'ADD_SEED_TO_FILE_NAMES no',
+
+    'DIMENSION 2',
+    "DIRECTION_TYPE ORTHO 2N",
+
+    'X0          ( 2.0  2.0 )',
+    'LOWER_BOUND ( 0.0  0.0 )',
+    'UPPER_BOUND ( 5.0  5.0 )',
+
+    'DISPLAY_DEGREE 0',
+
+    # 'DISPLAY_DEGREE 2',
+    # 'DISPLAY_STATS (BBE BLK_SIZE) [OBJ] (SOL) BBO',
+]
+
+# :)
+
+def program ():
+
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        # Construct the parallel evaluator
+        evaluator = functools.partial(executor.map, blackbox)
+
+        # Perform the optimization ritual.
+        result = nomadlad.minimize(evaluator, parameters) 
+
+    # Unpack and print out its result
+    flag, eval_count, best_feasible, best_infeasible = result
+
+    print('final termination flag           ', flag)
+    print('blackbox evaluation count        ', eval_count)
+    print('best   feasible result (f(w), w) ', best_feasible)
+    print('best infeasible result (f(w), w) ', best_infeasible)
+
+if __name__ == '__main__':
+    program()
+
